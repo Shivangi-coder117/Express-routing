4515cefe2efe01daef980c5beab63268ea98e631
Shivangi-coder117 <ss7433@srmist.edu.in>
2021-12-07 11:30:35 +0530

    DAtabase Relations

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,10 +1,28 @@
+const Post = require('../models/post');
+
 module.exports.home= function(req,res)
 {
   
-   res.cookie('user_id',25);
+   //res.cookie('user_id',25);
 
-   return res.render('home',{
-      title : "Home"
+   /*Post.find({},function(err,posts)
+   {
+      return res.render('home',{
+         title : "Codeial | Home"   ,
+         posts : posts
+   });
+
+   
+
+   });*/
+   //populate the user for each post
+
+   Post.find({}).populate('user').exec(function(err,posts)
+   {
+      return res.render('home',{
+         title : "Codeial | Home"   ,
+         posts : posts
+   });
 
    });
 }
