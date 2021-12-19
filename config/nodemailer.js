9a2a8624587d9b429d789c39ae50d2682f0ce11e
Shivangi-coder117 <ss7433@srmist.edu.in>
2021-12-19 19:18:24 +0530

    Nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path');
+
+
+
+
+let transporter = nodemailer.createTransport({
+    service : 'gmail',
+    host : 'smtp.gmail.com',
+    port : 587,
+    secure : false,
+    auth : {
+        user : 'alchemy.cn18',
+        pass : 'codingninjas'
+    }
+});
+
+
+ let renderTemplate = (data,relativePath) => {
+     let mailHTML ;
+     ejs.renderFile(
+            path.join(__dirname,'../views/mailers')
+     )
+ }
