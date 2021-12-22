ad7f8642b9d14d9ad35294bb39b776cf5f6a5707
Shivangi-coder117 <ss7433@srmist.edu.in>
2021-12-22 13:04:56 +0530

    Actions

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -63,7 +63,9 @@ module.exports.destroy = async function(req, res){
             comment.remove();
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
-
+               //destroy the associated likes
+               await Like.deleteMany({likeable : comment._id,onModel : 
+        'Comment'});
             // send the comment id which was deleted back to the views
             if (req.xhr){
                 return res.status(200).json({
